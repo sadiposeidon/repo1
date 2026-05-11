aef1740641256682eb0091f8a92e4706b7bf15b0
sadiposeidon <38244032+sadiposeidon@users.noreply.github.com>
2026-05-11 17:38:56 +0400

    .
    .

---
+++ l10n_eu_iot_scale_cert/controllers/expected_checksum.py
@@ -0,0 +1,5 @@
+# This checksum is based on the contents of the certified files contained
+# in this module (see controllers/checksum.py for the full list).
+# Any change to these files will require re-certification with LNE.
+# DO NOT CHANGE THEM OR THIS CHECKSUM WITHOUT CONTACTING THE POS TEAM FIRST!
+EXPECTED_CHECKSUM = "d49cb070ddc878043217f736e6f315a1ca8bae63796d30c8782789d8749394d6"
---
+++ spreadsheet_edition/static/src/assets/list_view/list_controller.js
@@ -0,0 +1,23 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { ListController } from "@web/views/list/list_controller";
+import { _t } from "@web/core/l10n/translation";
+
+export const patchListControllerExportSelection = {
+    getStaticActionMenuItems() {
+        const menuItems = super.getStaticActionMenuItems(...arguments);
+        menuItems["insert"] = {
+            sequence: 15,
+            icon: "oi oi-view-list",
+            description: _t("Insert in spreadsheet"),
+            callback: () => this.env.bus.trigger("insert-list-spreadsheet"),
+        };
+        return menuItems;
+    },
+};
+
+export const unpatchListControllerExportSelection = patch(
+    ListController.prototype,
+    patchListControllerExportSelection
+);
